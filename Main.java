684cc31b4b5276f6073cde40cc7b6618535ed091
shahwaiz345 <89724425+shahwaiz345@users.noreply.github.com>
2021-08-30 01:56:00 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of a");
+        float a = sc.nextFloat();
+        float A = a*a;
+        System.out.println(A);
+    }
+}
+//A = a*a
